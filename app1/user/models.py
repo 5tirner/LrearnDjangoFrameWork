807e6e2efc95ab0dddfd8beb6a7834265b9ee253
5tirner <thatfor1337@gmail.com>
2024-05-08 17:41:40 +0100

    zasabri

--- app1/user/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class   TheUser(models.Model):
-    Name = models.CharField(max_length=20)
-    age = models.CharField(max_length=3)
-    gender = models.CharField(max_length=10)
--- app1/user/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-
-def user(req):
-    # template = loader.get_template('index.html')
-    # return HttpResponse(template.render())
-    return HttpResponse("Five")
